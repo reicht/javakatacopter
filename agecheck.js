b5aeb8999a7e0c1ed7654695f75b76f8da058303
Jordan Corley <rainwolfj@gmail.com>
2016-04-12 14:00:51 -0500

    Hell yeah, adventurer array mastery achieved

--- agecheck.js
+++ agecheck.js
@@ -3,7 +3,16 @@ var check = function (n) {
   else console.log("Proceed");
 };
 
+// for(var i = 18; i < 24; i++) {
+//   check(i)
+// };
 
-for(var i = 18; i < 24; i++) {
-  check(i)
+var arrCheck = function (arr) {
+  arr.forEach( function (n) {
+    if (n < 21) console.log("Too Young");
+    else console.log("Proceed");
+  });
 };
+
+var ages = [18, 19, 20, 21, 22, 23, 24];
+arrCheck(ages);
